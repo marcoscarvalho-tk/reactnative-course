4766696c24f8860ae595d2b7b926dcbd4f468606
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2022-03-30 01:41:13 -0300

    update App.js add react-use-websocket package

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import useWebSocket from 'react-use-websocket';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [data, setData] = useState({});
+
+  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/btcusdt@ticker`, {
+    onOpen: () => { },
+    onMessage: () => {
+      if(lastJsonMessage){
+        setData(lastJsonMessage);
+      }
+    },
+    onError: (event) => alert(event),
+    shouldReconnect: () => true,
+    reconnectInterval: 3000
+
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Okay!! It's work fine!!</Text>
+      <Text>{JSON.stringify(data)}</Text>
       <StatusBar style="auto" />
     </View>
   );
